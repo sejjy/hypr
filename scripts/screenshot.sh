895e36a2435f6aea0214e3c124cd60d92b5f805f
sejjy <sejjymvm@gmail.com>
2025-01-03 23:09:28 +0800

    update config

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -3,17 +3,27 @@
 # save directory
 SCREENSHOT_DIR="$HOME/Pictures/Screenshots"
 
+# find the next available filename
+find_next() {
+  i=1
+  while [ -e "$SCREENSHOT_DIR/Screenshot ($i).png" ]; do
+    i=$((i + 1))
+  done
+  echo "Screenshot ($i).png"
+}
+
 # screenshot type
 case "$1" in
 "full")
   # full-screen screenshot
-  FILENAME="Screenshot ($(($(find "$SCREENSHOT_DIR" -maxdepth 1 -name 'Screenshot*.png' 2>/dev/null | wc -l) + 1))).png"
+  FILENAME=$(find_next)
   grimblast copysave screen "$SCREENSHOT_DIR/$FILENAME"
   ;;
 "partial")
   # partial screenshot with region selection
-  FILENAME="Screenshot ($(($(find "$SCREENSHOT_DIR" -maxdepth 1 -name 'Screenshot*.png' 2>/dev/null | wc -l) + 1))).png"
+  FILENAME=$(find_next)
   grimblast copysave area "$SCREENSHOT_DIR/$FILENAME"
+  # --freeze does not work
   ;;
 *)
   echo "Invalid argument"
